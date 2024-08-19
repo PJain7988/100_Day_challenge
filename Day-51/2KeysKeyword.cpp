e67fa9d0400587117feea9afbaaf1076dd5f6638
unknown <priyajain7988@gmail.com>
2024-08-19 19:11:50 +0530

    New Days

---
+++ Day-51/2KeysKeyword.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int minSteps(int n) {
+    if (n == 1)
+        return 0;
+    int steps = 0;
+    int factor = 2;
+    while (n > 1){
+        while (n % factor == 0){
+            steps += factor;
+            n/=factor;
+        }
+        factor += 1;
+    }  
+    return steps;
+}
+int main(){
+    int n;
+    cin >> n;
+    cout<<minSteps(n)<<endl;
+    return 0;
+}
